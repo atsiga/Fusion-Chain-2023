b526fb8d29da0297e1906cf551ec381338ef51d9
atsiga <atsiga@gmail.com>
2023-09-01 18:46:54 +0100

    Update network.py

--- network.js
+++ network.js
@@ -1,246 +1,183 @@
-"use strict";
-var pidusage = require('pidusage')
-var cpuUsage = "["
-var memUsage = "["
-const WebSocket = require("ws");
-const bc = require("./blockchain");
-var fs = require("fs");
-var newBlockFile  = new Array();
- 
- 
-var sockets = [];
-var MessageType = {
-    QUERY_LATEST: 0,
-    QUERY_ALL: 1,
-    RESPONSE_BLOCKCHAIN: 2,
-    REQUEST_PREVOTE: 3,
-    GET_PREVOTE: 4,
-    REQUEST_COMMIT: 5,
-    GET_COMMIT: 6
-};
-var consensus = false;  
-var initialPeers = process.env.PEERS ? process.env.PEERS.split(',') : [];
+import json
+import os
+import asyncio
+import websockets
 
-var p2p_port = process.env.P2P_PORT || 6001;
+from enum import Enum
 
-var getMessageCount = 0;
-var getValidationValue = 0;
+# Assuming you have network and blockchain modules in the same directory
+import network as nw
+import blockchain as bc
 
-var leader = false;
-var nodeNum = 0;
-var newBlock;
-var connectToPeers = (newPeers) => {
-    newPeers.forEach((peer) => {
-        var ws = new WebSocket(peer);
-        ws.on('open', () => initConnection(ws));
-        ws.on('error', () => {
-         });
-    });
-};
-var initP2PServer = () => {
-    var server = new WebSocket.Server({port: p2p_port});
-    server.on('connection', ws => initConnection(ws));
- 
-};
+sockets = []
+consensus = False
+initialPeers = os.environ.get('PEERS', '').split(',')
+p2p_port = int(os.environ.get('P2P_PORT', 6001))
 
-var initConnection = (ws) => {
-    sockets.push(ws);
-    initMessageHandler(ws);
-    initErrorHandler(ws);
-    write(ws, queryChainLengthMsg());
-};
-function minusNode() {
-    nodeNum--;
-}
+getMessageCount = 0
+getValidationValue = 0
+leader = False
+nodeNum = 0
+newBlock = None
 
+class MessageType(Enum):
+    QUERY_LATEST = 0
+    QUERY_ALL = 1
+    RESPONSE_BLOCKCHAIN = 2
+    REQUEST_PREVOTE = 3
+    GET_PREVOTE = 4
+    REQUEST_COMMIT = 5
+    GET_COMMIT = 6
 
-var initMessageHandler = (ws) => {
-    ws.on('message', (data) => {
-        var message = JSON.parse(data);
-  
-         switch (message.type) {
-            case MessageType.QUERY_LATEST:
-                write(ws, responseLatestMsg());
-                break;
-            case MessageType.QUERY_ALL:
-                write(ws, responseChainMsg());
-                break;
-            case MessageType.RESPONSE_BLOCKCHAIN:
-                handleBlockchainResponse(message);
-                break;
-             case MessageType.REQUEST_PREVOTE:  
-              
-                if (bc.isValidNewBlock(message.data, bc.getLatestBlock())) {
-                    broadcast(sendPreVoteMsg(message.data));
-                 }
-                else {
-                    broadcast(sendNotPreVoteMsg(message.data));
-                 }
+async def connectToPeers(newPeers):
+    for peer in newPeers:
+        async with websockets.connect(peer) as ws:
+            await initConnection(ws)
 
-                break;
+async def initP2PServer():
+    async with websockets.serve(initConnection, "0.0.0.0", p2p_port):
+        await asyncio.Future()
 
-            case MessageType.GET_PREVOTE:  
+async def initConnection(ws, path=None):
+    sockets.append(ws)
+    await initMessageHandler(ws)
+    await initErrorHandler(ws)
+    await write(ws, queryChainLengthMsg())
 
-                if(!leader) {
-                    nodeNum = sockets.length - 1;
-                    consensus = false;
-                     getMessageCount++;
-                    getValidationValue += message.count; 
-  
-                     if(nodeNum == getMessageCount) {
-           
+def minusNode():
+    global nodeNum
+    nodeNum -= 1
 
-                        if(nodeNum == getValidationValue) {
-                            consensus = true;
-                        }
-                    
-                        if(consensus) { 
+async def initMessageHandler(ws):
+    async for data in ws:
+        message = json.loads(data)
+        await handleMessage(ws, message)
 
-                            broadcast(RequestCOMMIT(message.data));
-                             getMessageCount = 0;
-                            getValidationValue = 0;
-                         }
-                        else { 
-                             getMessageCount = 0;
-                            getValidationValue = 0;
-                        }
-                    }
-                }
-                break;
- 
-                 case MessageType.REQUEST_COMMIT:
-                    if(!leader) {
-                  
-                        if (bc.isValidNewBlock(message.data, bc.getLatestBlock())) {
+async def handleMessage(ws, message):
+    global getMessageCount, getValidationValue, consensus, leader, nodeNum, newBlock
 
-                            broadcast(sendCommitMsg());
- 
-                        }
-                        else {
+    if message['type'] == MessageType.QUERY_LATEST.value:
+        await write(ws, responseLatestMsg())
+    elif message['type'] == MessageType.QUERY_ALL.value:
+        await write(ws, responseChainMsg())
+    elif message['type'] == MessageType.RESPONSE_BLOCKCHAIN.value:
+        await handleBlockchainResponse(message)
+    elif message['type'] == MessageType.REQUEST_PREVOTE.value:
+        if bc.isValidNewBlock(message['data'], bc.getLatestBlock()):
+            await broadcast(sendPreVoteMsg(message['data']))
+        else:
+            await broadcast(sendNotPreVoteMsg(message['data']))
+    elif message['type'] == MessageType.GET_PREVOTE.value:
+        if not leader:
+            nodeNum = len(sockets) - 1
+            consensus = False
+            getMessageCount += 1
+            getValidationValue += message['count']
+            
+            if nodeNum == getMessageCount:
+                if nodeNum == getValidationValue:
+                    consensus = True
+                
+                if consensus:
+                    await broadcast(RequestCOMMIT(message['data']))
+                    getMessageCount = 0
+                    getValidationValue = 0
+                else:
+                    getMessageCount = 0
+                    getValidationValue = 0
+    elif message['type'] == MessageType.REQUEST_COMMIT.value:
+        if not leader:
+            if bc.isValidNewBlock(message['data'], bc.getLatestBlock()):
+                await broadcast(sendCommitMsg())
+            else:
+                await broadcast(sendNotCommitMsg())
+                
+    elif message['type'] == MessageType.GET_COMMIT.value:
+        if not leader:
+            nodeNum = len(sockets)
+            consensus = False
+            getMessageCount += 1
+            getValidationValue += message['data']
+            
+            if nodeNum == getMessageCount:
+                if nodeNum == getValidationValue:
+                    consensus = True
+                
+                if consensus:
+                    bc.addBlock(bc.getNewBlock())
+                    bc.generateIPFSBlock(bc.getNewBlock())
+                    await broadcast(responseLatestMsg())
+                    getMessageCount = 0
+                    getValidationValue = 0
+                    leader = False
+                else:
+                    getMessageCount = 0
+                    getValidationValue = 0
 
-                            broadcast(sendNotCommitMsg());
-                         }
-                    } 
-                    break;
+async def initErrorHandler(ws):
+    async for _ in ws:
+        await closeConnection(ws)
+
+async def closeConnection(ws):
+    sockets.remove(ws)
+
+async def handleBlockchainResponse(message):
+    receivedBlocks = sorted(json.loads(message['data']), key=lambda x: x['index'])
+    latestBlockReceived = receivedBlocks[-1]
+    latestBlockHeld = bc.getLatestBlock()
     
-                case MessageType.GET_COMMIT:
-                    if(!leader) {
-                        nodeNum = sockets.length;
-                        consensus = false;
-                         getMessageCount++;
-                        getValidationValue += message.data; 
-           
-                         if(nodeNum == getMessageCount) {
-                   
-    
-                            if(nodeNum == getValidationValue) {
-                                consensus = true;
-                            }
-                        
-                            if(consensus) {
-                                bc.addBlock(bc.getNewBlock());
-                                 bc.generateIPFSBlock(bc.getNewBlock());
-                                broadcast(responseLatestMsg());
-                                 getMessageCount = 0;
-                                getValidationValue = 0;
-                                leader = false;
-                            }
-                            else {
-                                 getMessageCount = 0;
-                                getValidationValue = 0;
-                            }
-                        }
-                    }
-                     break;
-        }
-    });
-  
- 
-};
+    if latestBlockReceived['index'] > latestBlockHeld['index']:
+        if latestBlockHeld['hash'] == latestBlockReceived['previousHash']:
+            bc.addBlock(latestBlockReceived)
+            await broadcast(responseLatestMsg())
+        elif len(receivedBlocks) == 1:
+            await broadcast(queryAllMsg())
+        else:
+            await replaceChain(receivedBlocks)
 
-var initErrorHandler = (ws) => {
-    var closeConnection = (ws) => {
-         sockets.splice(sockets.indexOf(ws), 1);
-    };
-    ws.on('close', () => closeConnection(ws));
-    ws.on('error', () => closeConnection(ws));
-};
+def getSockets():
+    return sockets
 
-var handleBlockchainResponse = (message) => {
-    var receivedBlocks = JSON.parse(message.data).sort((b1, b2) => (b1.index - b2.index));
-    var latestBlockReceived = receivedBlocks[receivedBlocks.length - 1];
-    var latestBlockHeld = bc.getLatestBlock();
-    if (latestBlockReceived.index > latestBlockHeld.index) {
-         if (latestBlockHeld.hash === latestBlockReceived.previousHash) {
-             bc.addBlock(latestBlockReceived);
-            broadcast(responseLatestMsg());
-        } else if (receivedBlocks.length === 1) {
-             broadcast(queryAllMsg());
-        } else {
-             replaceChain(receivedBlocks);
-        }
-    } else {
-     }
-};
+def selectLeader(result):
+    global leader
+    leader = result
 
-function getSockets() { return sockets; }
+def queryChainLengthMsg():
+    return json.dumps({'type': MessageType.QUERY_LATEST.value})
 
-function selectLeader(result) {
-    leader = result;
-}
-var queryChainLengthMsg = () => ({'type': MessageType.QUERY_LATEST});
-var queryAllMsg = () => ({'type': MessageType.QUERY_ALL});
-var responseChainMsg = () =>({
-    'type': MessageType.RESPONSE_BLOCKCHAIN, 'data': JSON.stringify(blockchain)
-});
-var responseLatestMsg = () => ({
-    'type': MessageType.RESPONSE_BLOCKCHAIN,
-    'data': JSON.stringify([bc.getLatestBlock()])
-});
+def queryAllMsg():
+    return json.dumps({'type': MessageType.QUERY_ALL.value})
 
+def responseChainMsg():
+    return json.dumps({'type': MessageType.RESPONSE_BLOCKCHAIN.value, 'data': json.dumps(bc.getBlockchain())})
 
-var RequestPBFT = (newBlock) => ({ 
-    'type': MessageType.REQUEST_PREVOTE,
-    'data': newBlock
-});
-var RequestCOMMIT = (newBlock) => ({ 
-    'type': MessageType.REQUEST_COMMIT,
-    'data': newBlock
-});
-var sendPreVoteMsg = (newBlock) => ({
-    'type': MessageType.GET_PREVOTE,
-    'data' : newBlock,
-    'count' : 1
-});
-var sendNotPreVoteMsg = (newBlock) => ({
-    'type': MessageType.GET_PREVOTE,
-    'data' : newBlock,
-    'count' : 0
-});
+def responseLatestMsg():
+    return json.dumps({'type': MessageType.RESPONSE_BLOCKCHAIN.value, 'data': json.dumps([bc.getLatestBlock()])})
 
-var sendCommitMsg = () => ({
-    'type': MessageType.GET_COMMIT,
-    'data' : 1
+def RequestPBFT(newBlock):
+    return json.dumps({'type': MessageType.REQUEST_PREVOTE.value, 'data': newBlock})
 
-});
-var sendNotCommitMsg = () => ({
-    'type': MessageType.GET_COMMIT,
-    'data' : 0
-});
- 
-var write = (ws, message) => ws.send(JSON.stringify(message));
-var broadcast = (message) => sockets.forEach(
-function(socket){
-    write(socket, message)
- 
-});
+def RequestCOMMIT(newBlock):
+    return json.dumps({'type': MessageType.REQUEST_COMMIT.value, 'data': newBlock})
 
-module.exports = {
-    connectToPeers,
-    getSockets,
-    broadcast,
-    responseLatestMsg,
-    initP2PServer,
-    selectLeader,
-    RequestPBFT
- };
+def sendPreVoteMsg(newBlock):
+    return json.dumps({'type': MessageType.GET_PREVOTE.value, 'data': newBlock, 'count': 1})
+
+def sendNotPreVoteMsg(newBlock):
+    return json.dumps({'type': MessageType.GET_PREVOTE.value, 'data': newBlock, 'count': 0})
+
+def sendCommitMsg():
+    return json.dumps({'type': MessageType.GET_COMMIT.value, 'data': 1})
+
+def sendNotCommitMsg():
+    return json.dumps({'type': MessageType.GET_COMMIT.value, 'data': 0})
+
+async def write(ws, message):
+    await ws.send(message)
+
+async def broadcast(message):
+    await asyncio.gather(*[write(socket, message) for socket in sockets])
+
+if __name__ == "__main__":
+    nw.connectToPeers(initialPeers)
+    asyncio.run(initP2PServer())
